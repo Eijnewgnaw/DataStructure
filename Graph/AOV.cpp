2abf4b8d1437559f72712173c874ffb824702913
PC <2712350454@qq.com>
2023-06-02 13:31:28 +0800

    reorganise

--- Graph/AOV.cpp
+++ Graph/AOV.cpp
@@ -1,38 +1,31 @@
 //拓扑排序
 #include <iostream>
-#include <vector>
-#include <queue>
-using namespace std;
 
-const int MAXN = 100;
-vector<int> G[MAXN];
-int inDegree[MAXN];
-int n, m;
+int print[MAX_VERTEX_NUM];  //存放拓扑排序结果
+int inDegree[MAXN_VERTEX_NUM];  //存放每个点的入度
 
 bool topologicalSort() {
-    int num = 0;
-    queue<int> q;
-    for(int i = 0; i < n; i++) {
-        if(inDegree[i] == 0) {
-            q.push(i);
+   InitStack(S);
+    for (int i = 0; i < n; i++) {
+        if (inDegree[i] == 0) {
+            Push(S, i);
         }
     }
-    while(!q.empty()) {
-        int u = q.front();
-        q.pop();
-        for(int i = 0; i < G[u].size(); i++) {
-            int v = G[u][i];
-            inDegree[v]--;
-            if(inDegree[v] == 0) {
-                q.push(v);
+    int count = 0;
+    while (!IsEmpty(S)) {
+        Pop(S,i);
+        print[count++]=i;   //将弹出的顶点加入拓扑排序结果中
+        for (p=G.vertices[i].firstarc; p; p=p->nextarc) {
+            v = p->adjvex;      //找到顶点v的邻接点
+            inDegree[v]--;    //将顶点v的邻接点的入度减1
+            if (inDegree[v] == 0) {
+                Push(S, v);
             }
         }
-        G[u].clear();       //清空该点的所有出边
-        num++;
     }
-    if(num == n) {
-        return true;
-    } else {
+    if (count < G.vexnum) {
         return false;
+    } else {
+        return true;
     }
 }
